cc22c8fd69b6dd70f1b3e6082d2d77ac9f41c281
ThinaTaliweGBA <thina.taliwecode2@gmail.com>
2024-09-04 15:25:55 +0200

    Initial Spring Boot project setup

---
+++ src/main/java/com/enviro/assessment/grad001/thinataliwe/ThinataliweApplication.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.thinataliwe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThinataliweApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThinataliweApplication.class, args);
+	}
+
+}
